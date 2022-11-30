44691687f6616de9e6a6da67e36aa0b5cbced1e9
ibrammalik <malikmaul1173@gmail.com>
2022-11-30 21:57:17 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // override hostname because windows 7
-const os = require('os');
+// const os = require('os');
 
-os.hostname = () => 'localhost';
+// os.hostname = () => 'localhost';
 
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
@@ -9,7 +9,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
