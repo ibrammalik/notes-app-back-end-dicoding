07dcc5c9972319c22b42e5014b4544691ee746e5
ibrammalik <malikmaul1173@gmail.com>
2022-11-30 21:50:32 +0700

    add files

---
+++ src/server.js
@@ -0,0 +1,26 @@
+// override hostname because windows 7
+const os = require('os');
+
+os.hostname = () => 'localhost';
+
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  server.route(routes);
+
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
